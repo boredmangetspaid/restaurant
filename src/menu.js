39984399f3be2947eda7a8c048897d7900e2c79d
boredmangetspaid <develop202234@gmail.com>
2025-10-25 20:08:51 -0400

    added classic bagels to menu

--- src/menu.js
+++ src/menu.js
@@ -1,40 +1,35 @@
 import { footer } from "./footer"
 import { menuItem } from "./menuItem.js"
 import plainBagelImage from "./assets/plain.png"
+import multiBagelImage from "./assets/multigrain.png"
+import PoppyBagelImage from "./assets/poppy.png"
+import cinnamonBagelImage from "./assets/cinnamon_raisin.png"
+import sesameBagelImage from "./assets/sesame.png"
+
 export const loadMenu = function loadMenu() {
    const content = document.getElementById("content")
    //container for menu
    const menuContainer = document.createElement("div");
    menuContainer.id = "menuContainer";
 
-   //menu items
-   const plainBagel = menuItem("Classic Plain Bagel", "$7.60", plainBagelImage)
-   menuContainer.appendChild(plainBagel)
+   //classic bagels
+   const plainBagel = menuItem("Classic Plain Bagel", "$2.50", plainBagelImage);
+   const multiGrainBagel= menuItem("Multi Grain Bagel", "$2.50", multiBagelImage);
+   const poppyBagel= menuItem("Classic Plain Bagel", "$2.50", PoppyBagelImage);
+   const cinnamonRaisinBagel= menuItem("Classic Plain Bagel", "$2.50", cinnamonBagelImage);
+   const sesameBagel= menuItem("Classic Plain Bagel", "$2.50", sesameBagelImage);
 
-   const plainBagel2 = menuItem("Classic Plain Bagel", "$7.60", plainBagelImage)
-   menuContainer.appendChild(plainBagel2)
+   const bagelMenu = [plainBagel,multiGrainBagel,poppyBagel,cinnamonRaisinBagel,sesameBagel];
+   
+   for (const bagel of bagelMenu){
+      menuContainer.appendChild(bagel)
+   }
 
-   const plainBagel3 = menuItem("Classic Plain Bagel", "$7.60", plainBagelImage)
-   menuContainer.appendChild(plainBagel3)
+   //bagel sandwiches
+  
 
-   const plainBagel4 = menuItem("Classic Plain Bagel", "$7.60", plainBagelImage)
-   menuContainer.appendChild(plainBagel4)
 
-   const plainBagel5 = menuItem("Classic Plain Bagel", "$7.60", plainBagelImage)
-   menuContainer.appendChild(plainBagel5)
-
-   const plainBagel6 = menuItem("Classic Plain Bagel", "$7.60", plainBagelImage)
-   menuContainer.appendChild(plainBagel6)
-
-   const plainBagel7 = menuItem("Classic Plain Bagel", "$7.60", plainBagelImage)
-   menuContainer.appendChild(plainBagel7)
-
-   const plainBagel8 = menuItem("Classic Plain Bagel", "$7.60", plainBagelImage)
-   menuContainer.appendChild(plainBagel8)
-
-   const plainBagel9 = menuItem("Classic Plain Bagel", "$7.60", plainBagelImage)
-   menuContainer.appendChild(plainBagel9)
-
+   
 
    content.appendChild(menuContainer)
    footer()
